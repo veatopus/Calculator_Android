c76696c37e857d704402985ce26b44216292de1c
veatopus <veatopus@gmail.com>
2020-04-30 20:09:21 +0600

    I need help

--- app/src/main/java/com/example/calculator_android/MainActivity.java
+++ app/src/main/java/com/example/calculator_android/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     String intermediateNumber = "";
     TextView textView;
     Double firstNumber, secondNumber;
-    String operation;
-
+    String operation, result;
+    boolean isFirst, resetAfterOperation = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +23,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tappingNumber(View view) {
-
+        if (resetAfterOperation) {
+            textView.setText("");
+            resetAfterOperation = !resetAfterOperation;
+        }
         switch (view.getId()) {
             case R.id.button0:
                 textView.append("0");
                 intermediateNumber += "0";
+                isFirst = true;
                 break;
             case R.id.button1:
                 textView.append("1");
                 intermediateNumber += "1";
+                isFirst = true;
                 break;
             case R.id.button2:
                 textView.append("2");
@@ -40,30 +45,44 @@ public class MainActivity extends AppCompatActivity {
             case R.id.button3:
                 textView.append("3");
                 intermediateNumber += "3";
+                isFirst = true;
                 break;
             case R.id.button4:
                 textView.append("4");
                 intermediateNumber += "4";
+                isFirst = true;
+                break;
+            case R.id.dot:
+                if (isFirst) {
+                    textView.append(".");
+                    intermediateNumber += ".";
+                    isFirst = !isFirst;
+                }
                 break;
             case R.id.button5:
                 textView.append("5");
                 intermediateNumber += "5";
+                isFirst = true;
                 break;
             case R.id.button6:
                 textView.append("6");
                 intermediateNumber += "6";
+                isFirst = true;
                 break;
             case R.id.button7:
                 textView.append("7");
                 intermediateNumber += "7";
+                isFirst = true;
                 break;
             case R.id.button8:
                 textView.append("8");
                 intermediateNumber += "8";
+                isFirst = true;
                 break;
             case R.id.button9:
                 textView.append("9");
                 intermediateNumber += "9";
+                isFirst = true;
                 break;
         }
 
@@ -73,71 +92,94 @@ public class MainActivity extends AppCompatActivity {
     public void tappingLogic(View view) {
         switch (view.getId()) {
             case R.id.button_addition:
-                firstNumber = Double.valueOf(intermediateNumber);
-                operation = "+";
-                textView.setText("");
-                intermediateNumber = String.valueOf(0);
+                if (isFirst) {
+                    firstNumber = Double.valueOf(intermediateNumber);
+                    operation = "+";
+                    textView.setText("");
+                    intermediateNumber = null;
+                }
                 break;
 
             case R.id.button_subtraction:
-                firstNumber = Double.valueOf(intermediateNumber);
-                operation = "-";
-                textView.setText("");
-                intermediateNumber = String.valueOf(0);
+                if (isFirst) {
+                    firstNumber = Double.valueOf(intermediateNumber);
+                    operation = "-";
+                    textView.setText("");
+                    intermediateNumber = null;
+                }
                 break;
 
             case R.id.button_multiplication:
-                firstNumber = Double.valueOf(intermediateNumber);
-                operation = "*";
-                textView.setText("");
-                intermediateNumber = String.valueOf(0);
+                if (isFirst) {
+                    firstNumber = Double.valueOf(intermediateNumber);
+                    operation = "*";
+                    textView.setText("");
+                    intermediateNumber = null;
+                }
                 break;
 
             case R.id.button_division:
-                firstNumber = Double.valueOf(intermediateNumber);
-                operation = "/";
-                textView.setText("");
-                intermediateNumber = String.valueOf(0);
+                if (isFirst) {
+                    firstNumber = Double.valueOf(intermediateNumber);
+                    operation = "/";
+                    textView.setText("");
+                    intermediateNumber = null;
+                }
+                break;
+
+            case R.id.back:
+
                 break;
 
             case R.id.clear:
-                intermediateNumber = "";
-                firstNumber = (double) 0;
-                secondNumber = (double) 0;
-                operation = "";
-                textView.setText(" ");
+                clear();
+                textView.setText("");
                 break;
 
             case R.id.button_equals:
-                secondNumber = Double.valueOf(intermediateNumber);
-                textView.setText("");
+                if (operation != null) {
+                    secondNumber = Double.valueOf(intermediateNumber);
+                    textView.setText("");
 
-                switch (operation) {
-                    case "+":
-                        textView.setText(Double.parseDouble(String.valueOf(firstNumber))
-                                + Double.parseDouble(String.valueOf(secondNumber)) + "");
-                        break;
+                    switch (operation) {
+                        case "+":
+                            result = Double.parseDouble(String.valueOf(firstNumber))
+                                    + Double.parseDouble(String.valueOf(secondNumber)) + "";
+                            break;
 
-                    case "-":
-                        textView.setText(Double.parseDouble(String.valueOf(firstNumber))
-                                - Double.parseDouble(String.valueOf(secondNumber)) + "");
-                        break;
+                        case "-":
+                            result = Double.parseDouble(String.valueOf(firstNumber))
+                                    - Double.parseDouble(String.valueOf(secondNumber)) + "";
+                            break;
 
-                    case "*":
-                        textView.setText(Double.parseDouble(String.valueOf(firstNumber))
-                                * Double.parseDouble(String.valueOf(secondNumber)) + "");
-                        break;
+                        case "*":
+                            result = Double.parseDouble(String.valueOf(firstNumber))
+                                    * Double.parseDouble(String.valueOf(secondNumber)) + "";
+                            break;
 
-                    case "/":
-                        textView.setText(Double.parseDouble(String.valueOf(firstNumber))
-                                / Double.parseDouble(String.valueOf(secondNumber)) + "");
-                        break;
+                        case "/":
+                            result = Double.parseDouble(String.valueOf(firstNumber))
+                                    / Double.parseDouble(String.valueOf(secondNumber)) + "";
+                            break;
+                    }
+                    resetAfterOperation = true;
+                    textView.setText(result);
+                    clear();
+
                 }
-                operation = "";
-                firstNumber = (double) 0;
-                secondNumber = (double) 0;
+
                 break;
         }
 
+    }
+
+    public void clear() {
+        intermediateNumber = null;
+        result = null;
+        firstNumber = null;
+        secondNumber = null;
+        operation = null;
+        resetAfterOperation = false;
+        isFirst = false;
     }
 }
